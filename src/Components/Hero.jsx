934e5103273bac21eadf187d317ef74f95d833c2
web-dev <webdev6190@gmail.com>
2024-11-18 10:45:42 -0800

    updated

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -18,33 +18,33 @@ const Hero = () => {
       <div className=' text-center'>
       <h1 className='mt-[2rem] text-center cursor-pointer text-transparent bg-clip-text bg-gradient-to-r
        from-green-500 to-blue-500 text-4xl font-bold mb-4'>Dev. Celestine E.O  </h1> 
-      <span className=' cursor-pointer text-white to-blue-500 text-md'>A Frontend Developer with experience in;</span>
+      <span className=' cursor-pointer text-white to-blue-500 text-md'>A Frontend Developer with vast experience in;</span>
       <p className=' text-md text-gray-400'>(HTML, CSS, Javascript, Reactjs & Tailwindcss)</p>
       <p className=' text-lg text-gray-300'> Specialize in buiding modern and responsive web application.</p>
       </div>
     </div>
     </div>
 
-<div className=' md:flex md:space-x-10 md:justify-center items-center justify-items-center space-y-10 '>
-      <div className='  w-[20rem] p-2 rounded-lg items-center flex mb-5  bg-gray-600'>
+<div className=' md:flex md:justify-center md:items-center md:space-x-16 justify-items-center'>
+      <div className='  w-[20rem] p-2 rounded-lg items-center flex mb-5  bg-gray-600 my-2'>
        <p className='hover:text-gray-400/90 ml-3 text-gray-200'>Resume</p>
        <button className=' cursor-pointer text-gray-100 bg-gradient-to-r
        from-green-500 to-blue-500 text-sm ml-[9rem] py-1 px-1.5 rounded-lg
-        transition-transform duration-300 hover:scale-105 '><a href="/src/Documents/Resume.pdf" download>Download</a></button>
+        transition-transform duration-300 hover:scale-105 '><a href="/src/Documents/Resume.pdf"download>Download</a></button>
       </div>
 
-      <div className='  w-[20rem] p-2 rounded-lg items-center flex mb-5  bg-gray-600'>
+      <div className='  w-[20rem] p-2 rounded-lg items-center flex mb-5  bg-gray-600 my-2'>
        <p className='hover:text-gray-400/90 ml-3 text-gray-200'>I.T Cert</p>
        <button className=' cursor-pointer text-gray-100 bg-gradient-to-r
        from-green-500 to-blue-500 text-sm ml-[9rem] py-1 px-1.5 rounded-lg
-        transition-transform duration-300 hover:scale-105 '><a href="/src/assets/IT.png" download>Download</a></button>
+        transition-transform duration-300 hover:scale-105 '><a href="/src/assets/IT.png"download>Download</a></button>
       </div>
 
-      <div className='  w-[20rem] p-2 rounded-lg items-center flex mb-5  bg-gray-600'>
+      <div className='  w-[20rem] p-2 rounded-lg items-center flex mb-5  bg-gray-600 my-2'>
        <p className='hover:text-gray-400/90 ml-3 text-gray-200'>Degree</p>
        <button className=' cursor-pointer text-gray-100 bg-gradient-to-r
        from-green-500 to-blue-500 text-sm ml-[9rem] py-1 px-1.5 rounded-lg
-        transition-transform duration-300 hover:scale-105 '><a href="/src/assets/portfolioImg.JPG" download>Download</a></button>
+        transition-transform duration-300 hover:scale-105 '><a href="/src/assets/portfolioImg.JPG"download>Download</a></button>
       </div>
 </div>
     
