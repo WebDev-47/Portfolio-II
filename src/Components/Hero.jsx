4a689b86c219f865026ab943a761e879f5f2ce15
web-dev <webdev6190@gmail.com>
2024-11-18 03:42:29 -0800

    update

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
     <div>
        <div className=" text-white py-20">
     <div className="container mx-auto px-8 md:px-16 lg:px-24">
-      <h2 className="text-4xl font-bold text-center mb-6 text-gray-300">My Portfolio<hr className="mt-4 animate-pulse"/></h2>
+      <h2 className="text-4xl font-bold text-center mb-6 text-gray-300">My Portfolio<hr className="mt-3 animate-pulse"/></h2>
       
       <div className=' flex justify-center items-center '>
       <img className=' h-[16rem] w-[16rem] rounded-full object-cover 
@@ -21,6 +21,7 @@ const Hero = () => {
       <p className=' text-md text-gray-400'>(HTML, CSS, Javascript, Reactjs & Tailwindcss)</p>
       <p className=' text-lg text-gray-300'> Specialize in buiding modern and responsive web application.</p>
       </div>
+      
      
     </div>
    
